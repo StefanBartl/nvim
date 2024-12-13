c5250a3b4d64dcf804586ff278a1bb3638f3d42a
l.value <bartl.stefanwien@gmail.com>
2024-12-13 16:39:01 +0100

    Ready for LSP & Language implementation

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,10 +1,210 @@
 require "nvchad.mappings"
 
--- add yours here
-
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- Utils
+-- Normal mode keymaps
+map("n", "<leader>uz", ":echo len(join(getline(1, '$'), ''))<CR>", { desc = "Zeichen zählen" })
+map("n", "<leader>uw", ":echo len(split(join(getline(1, '$'), ''), '\\s\\+'))<CR>", { desc = "Wörter zählen" })
+
+map("n", "copyz", ':let @+=getline(".")<CR>:echo "Line copied to clipboard"<CR>', { desc = "Zeile in die Zwischenablage kopieren" })
+map("n", "copyc", ':.,$y+<CR>:echo "Copied to clipboard from cursor to EOF"<CR>', { desc = "Von Cursor bis EOF in die Zwischenablage kopieren" })
+map("n", "copyf", ':%y+<CR>:echo "Copied entire file to clipboard"<CR>', { desc = "Gesamte Datei in die Zwischenablage kopieren" })
+map("v", "copys", '"+y<CR>:echo "Copied selected text to clipboard"<CR>', { desc = "Ausgewählten Text in die Zwischenablage kopieren" })
+
+map("n", "<leader>ex", ":bufdo bd | qa<CR>", { desc = "Alle Buffer schließen und Neovim beenden" })
+map("n", "<leader>del", ":lua confirm_delete()<CR>", { desc = "Aktuelle Datei löschen (mit Bestätigung)" })
+map("n", "<leader>d!!", ":call DeleteFile()<CR>", { desc = "Datei löschen und Buffer schließen (ohne Bestätigung)" })
+
+map("n", "<CR>", function()
+  -- Speichert die aktuelle Cursorposition
+  local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+  -- Fügt oberhalb der aktuellen Zeile eine neue Zeile ein
+  vim.api.nvim_buf_set_lines(0, row - 1, row - 1, false, { "" })
+  -- Bewegt den Cursor in die ursprüngliche Zeile
+  vim.api.nvim_win_set_cursor(0, { row, col })
+end, { desc = "Zeile oberhalb einfügen" })
+
+
+-- Toggleterm
+-- Terminal mode keymaps
+map("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
+
+
+
+-- Nvimtree
+map("n", "<leader>+", function() vim.cmd("vertical resize +5") end, { desc = "Increase NvimTree width by 5" })
+map("n", "<leader>-", function() vim.cmd("vertical resize -5") end, { desc = "Decrease NvimTree width by 5" })
+
+
+-- Quixkfix
+map("n", "<leader>qo", ":copen<CR>", { desc = "Quickfix: Öffne Quickfix-Fenster" })
+map("n", "<leader>qn", ":cnext<CR>", { desc = "Quickfix: Gehe zum nächsten Fehler" })
+map("n", "<leader>qp", ":cprev<CR>", { desc = "Quickfix: Gehe zum vorherigen Fehler" })
+map("n", "<leader>qe", ":clist<CR>", { desc = "Quickfix: Zeige die Fehlerliste" })
+
+
+-- Treesitter
+map("n", "<leader>nf", "]m", { desc = "Nächste Funktion" })
+map("n", "<leader>pf", "[m", { desc = "Vorherige Funktion" })
+map("n", "<leader>nc", "]C", { desc = "Nächste Klasse" })
+map("n", "<leader>pc", "[C", { desc = "Vorherige Klasse" })
+map("n", "<leader>yf", "yaf", { desc = "Ganze Funktion kopieren" })
+map("n", "<leader>yc", "yac", { desc = "Ganze Klasse kopieren" })
+map("n", "<leader>df", "daf", { desc = "Ganze Funktion löschen" })
+map("n", "<leader>dc", "dac", { desc = "Ganze Klasse löschen" })
+map("n", "<leader>xf", "xaf", { desc = "Ganze Funktion ausschneiden" })
+map("n", "<leader>xc", "xac", { desc = "Ganze Klasse ausschneiden" })
+map("n", "<leader>if", "vif", { desc = "Inneren Funktionsinhalt auswählen" })
+map("n", "<leader>ic", "vic", { desc = "Inneren Klasseninhalt auswählen" })
+map("n", "<leader>ab", "vab", { desc = "Äußeren Block auswählen" })
+map("n", "<leader>ib", "vib", { desc = "Inneren Block auswählen" })
+map("n", "<leader>nb", "]b", { desc = "Nächsten Block finden" })
+map("n", "<leader>pb", "[b", { desc = "Vorherigen Block finden" })
+map("n", "<leader>np", "]p", { desc = "Nächsten Parameter finden" })
+map("n", "<leader>pp", "[p", { desc = "Vorherigen Parameter finden" })
+map("n", "<leader>ip", "vip", { desc = "Inneren Parameter auswählen" })
+map("n", "<leader>ap", "vap", { desc = "Äußeren Parameter auswählen" })
+map("x", "af", "@function.outer", { desc = "Äußere Funktion im visuellen Modus auswählen" })
+map("x", "if", "@function.inner", { desc = "Innere Funktion im visuellen Modus auswählen" })
+map("x", "ac", "@class.outer", { desc = "Äußere Klasse im visuellen Modus auswählen" })
+map("x", "ic", "@class.inner", { desc = "Innere Klasse im visuellen Modus auswählen" })
+
+
+-- Telescope
+-- Normal mode keymaps
+map("n", "<leader>ff", function()
+  require("telescope.builtin").find_files()
+end, { desc = "Find Files" })
+map("n", "<leader>fg", function()
+require("telescope.builtin").live_grep()
+end, { desc = "Live Grep" })
+map("n", "<leader><leader>", "<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>", { desc = "Find all" })
+map("n", "<leader>git", "<cmd> Telescope git_files <CR>", { desc = "Öffne Git-Dateien" })
+map("n", "<Space>gf", "<cmd> Telescope live_grep <CR>", { desc = "Live-Grep" })
+map("n", "<Space>fh", "<cmd> Telescope help_tags <CR>", { desc = "Hilfe-Tags durchsuchen" })
+map("n", "<leader>pf", "<cmd> Telescope find_files <CR>", { desc = "Dateien finden" })
+map("n", "<Space>comm", "<cmd> Telescope git_commits <CR>", { desc = "Git-Commits anzeigen" })
+map("n", "<leader>gs", function()
+require("telescope.builtin").grep_string({ search = vim.fn.input("Grep > ") })
+end, { desc = "Grep-Suche" })
+map("n", "<leader>tu", ":Telescope<CR>", { desc = "Telescope UI starten" })
+-- Insert mode keymaps
+map("i", "<M-p>", "<Cmd>lua require('telescope.builtin').find_files()<CR>", { desc = "Vorherige Suchkategorie" })
+map("i", "<M-n>", "<Cmd>lua require('telescope.builtin').find_files()<CR>", { desc = "Nächste Suchkategorie" })
+
+
+-- Harpoon
+-- Normal mode keymaps
+map("n", "<leader>h", function() require("harpoon.mark").add_file() end, { desc = "Datei zu Harpoon hinzufügen" })
+map("n", "<C-h>", function() require("harpoon.ui").toggle_quick_menu() end, { desc = "Harpoon-Menü umschalten" })
+map("n", "<leader>1", function() require("harpoon.ui").nav_file(1) end, { desc = "Navigiere zu Harpoon Datei 1" })
+map("n", "<leader>2", function() require("harpoon.ui").nav_file(2) end, { desc = "Navigiere zu Harpoon Datei 2" })
+map("n", "<leader>3", function() require("harpoon.ui").nav_file(3) end, { desc = "Navigiere zu Harpoon Datei 3" })
+map("n", "<leader>4", function() require("harpoon.ui").nav_file(4) end, { desc = "Navigiere zu Harpoon Datei 4" })
+map("n", "<leader>5", function() require("harpoon.ui").nav_file(5) end, { desc = "Navigiere zu Harpoon Datei 5" })
+map("n", "<leader>6", function() require("harpoon.ui").nav_file(6) end, { desc = "Navigiere zu Harpoon Datei 6" })
+map("n", "<leader>7", function() require("harpoon.ui").nav_file(7) end, { desc = "Navigiere zu Harpoon Datei 7" })
+map("n", "<leader>8", function() require("harpoon.ui").nav_file(8) end, { desc = "Navigiere zu Harpoon Datei 8" })
+map("n", "<leader>9", function() require("harpoon.ui").nav_file(9) end, { desc = "Navigiere zu Harpoon Datei 9" })
+
+
+-- Diffview
+map("n", "<leader>dv", ":DiffviewOpen<CR>", { desc = "Diffview öffnen" })
+map("n", "<leader>dc", ":DiffviewClose<CR>", { desc = "Diffview schließen" })
+map("n", "<leader>dh", ":DiffviewFileHistory<CR>", { desc = "Dateihistorie in Diffview anzeigen" })
+
+
+-- Fugitive
+-- Normal mode keymaps
+map("n", "<leader>gf", ":Git<CR>", { desc = "Git-Status anzeigen (Fugitive)" })
+map("n", "<leader>gk", ":Git commit<CR>", { desc = "Git-Commit erstellen (Fugitive)" })
+map("n", "<leader>gi", ":Gdiffsplit<CR>", { desc = "Git-Diff im Split anzeigen (Fugitive)" })
+map("n", "<leader>gb", ":Git blame<CR>", { desc = "Git-Blame anzeigen (Fugitive)" })
+map("n", "<leader>gs", ":Gstatus<CR>", { desc = "Git: Zeige den Git-Status" })
+map("n", "<leader>gd", ":Gdiff<CR>", { desc = "Git: Zeige die Unterschiede" })
+map("n", "<leader>gc", ":Gcommit<CR>", { desc = "Git: Führe einen Git-Commit durch" })
+map("n", "<leader>gl", ":Glog<CR>", { desc = "Git: Zeige das Git-Log" })
+map("n", "<leader>gp", ":Gpush<CR>", { desc = "Git: Pushe Änderungen zum Remote-Repository" })
+map("n", "<leader>gu", ":Gpull<CR>", { desc = "Git: Hole die neuesten Änderungen" })
+
+
+-- Neogit
+map("n", "<leader>ng", function() require("neogit").open() end, { desc = "Neogit Interface öffnen" })
+map("n", "<leader>nc", function() require("neogit").open({ "commit" }) end, { desc = "Neogit Commit" })
+
+
+-- Markdown
+map("i", "<C-Right>", "<Esc>zai", { desc = "Toggle folding under cursor" })
+map("i", "<C-Down>", "<Esc>zRi", { desc = "Open all foldings" })
+map("i", "<C-Left>", "<Esc>zci", { desc = "Close folding under cursor" })
+map("i", "<C-Up>", "<Esc>zMi", { desc = "Close all foldings" })
+map("n", "<leader>fo", "za", { desc = "Toggle Fold unter dem Cursor" }) -- Fold unter dem Cursor ein-/ausklappen
+map("n", "<leader>fa", "ggVGzM", { desc = "Alle Headings falten" })     -- Alle Headings falten
+
+
+-- Docker
+-- Normal mode keymaps
+-- Allgemeine Docker-Befehle
+map("n", "<leader>dp", ":DockerToolsOpen<CR>", { desc = "Show Docker containers" })
+map("n", "<leader>di", ":DockerImages<CR>", { desc = "Show Docker images" })
+-- DockerTools Panel
+map("n", "<leader>do", ":DockerToolsOpen<CR>", { desc = "Open DockerTools Panel" })
+map("n", "<leader>dc", ":DockerToolsClose<CR>", { desc = "Close DockerTools Panel" })
+map("n", "<leader>dt", ":DockerToolsToggle<CR>", { desc = "Toggle DockerTools Panel" })
+-- Container-Befehle
+map("n", "<leader>cs", ":ContainerStart ", { desc = "Start a Docker container" })
+map("n", "<leader>cp", ":ContainerStop ", { desc = "Stop a Docker container" })
+map("n", "<leader>cr", ":ContainerRemove ", { desc = "Remove a Docker container" })
+map("n", "<leader>cl", ":ContainerLogs ", { desc = "Show logs for Docker container" })
+-- Benutzerdefinierte Docker-Befehle
+vim.api.nvim_create_user_command("DockerLogs", function(opts)
+  vim.cmd("split | term docker logs " .. opts.args)
+end, { nargs = 1, desc = "Show Docker logs for a container" })
+
+vim.api.nvim_create_user_command("DockerExec", function(opts)
+  vim.cmd("split | term docker exec -it " .. opts.args)
+end, { nargs = 1, desc = "Execute command in a Docker container" })
+
+vim.api.nvim_create_user_command("DockerRm", function(opts)
+  vim.cmd("split | term docker rm " .. opts.args)
+end, { nargs = 1, desc = "Remove a Docker container" })
+-- Keymaps für benutzerdefinierte Docker-Befehle
+map("n", "<leader>dl", ":DockerLogs ", { desc = "Show Docker logs (type container name)" })
+map("n", "<leader>de", ":DockerExec ", { desc = "Execute command in Docker container" })
+map("n", "<leader>dr", ":DockerRm ", { desc = "Remove Docker container (type name)" })
+
+
+-- Copilot
+-- Insert mode keymaps
+map("i", "<C-y>", "<Plug>(copilot-accept)", { desc = "Accept Copilot suggestion" })
+map("i", "<C-l>", "<Plug>(copilot-accept-word)", { desc = "Accept next word of suggestion" })
+map("i", "<C-k>", "<Plug>(copilot-accept-line)", { desc = "Accept next line of suggestion" })
+map("i", "<C-r>", "<Plug>(copilot-dismiss)", { desc = "Dismiss suggestion" })
+map("i", "<C-n>", "<Plug>(copilot-next)", { desc = "Cycle to next suggestion" })
+map("i", "<C-p>", "<Plug>(copilot-previous)", { desc = "Cycle to previous suggestion" })
+map("i", "<C-s>", "<Plug>(copilot-suggest)", { desc = "Explicitly request suggestion" })
+
+-- FZF
+-- Normal mode keymaps
+map("n", "<leader>fza", ":FzfLua commands<CR>", { desc = "Befehle durchsuchen" })
+map("n", "<leader>fzb", ":FzfLua buffers<CR>", { desc = "Buffer durchsuchen" })
+map("n", "<leader>color", ":FzfLua colorschemes<CR>", { desc = "Farbschemata durchsuchen" })
+map("n", "<leader>fzd", ":FzfLua git_status<CR>", { desc = "Git-Status anzeigen" })
+map("n", "<leader>fze", ":FzfLua files<CR>", { desc = "Dateien durchsuchen" })
+map("n", "<leader>fzg", ":FzfLua git_commits<CR>", { desc = "Git-Commits durchsuchen" })
+map("n", "<leader>fzh", ":FzfLua help_tags<CR>", { desc = "Hilfe-Tags durchsuchen" })
+map("n", "<leader>fzi", ":FzfLua git_files<CR>", { desc = "Git-Dateien durchsuchen" })
+map("n", "<leader>fzj", ":FzfLua jumps<CR>", { desc = "Sprungpunkte durchsuchen" })
+map("n", "<leader>fzk", ":FzfLua lines<CR>", { desc = "Zeilen durchsuchen" })
+map("n", "<leader>reg", ":FzfLua registers<CR>", { desc = "Register durchsuchen" })
+map("n", "<leader>fzm", ":FzfLua quickfix<CR>", { desc = "Quickfix-Liste durchsuchen" })
+map("n", "<leader>fzr", ":FzfLua changes<CR>", { desc = "Änderungen durchsuchen" })
+map("n", "<leader>fz5", ":FzfLua command_history<CR>", { desc = "Befehlshistorie durchsuchen" })
+map("n", "<leader>grep", ":FzfLua live_grep<CR>", { desc = "Live-Grep" })
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,4 +1,119 @@
 return {
+
+-- 1. Essential dependencies and tools
+
+  -- Plenary, a necessary dependency for many other plugins
+  "nvim-lua/plenary.nvim",
+  -- Telescope for fuzzy finding files, buffers, etc.
+  {
+    "nvim-telescope/telescope.nvim",
+    lazy = false,
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-lua/plenary.nvim" },
+    cmd = "Telescope",
+    -- TODO: Wenn trouble installiert ist
+    --opts = function()
+    --  return require "configs.telescope"
+    --end,
+    config = function(_, opts)
+      dofile(vim.g.base46_cache .. "telescope")
+      local telescope = require "telescope"
+      telescope.setup(opts)
+      -- Load extensions
+      for _, ext in ipairs(opts.extensions_list) do
+        telescope.load_extension(ext)
+      end
+    end,
+  },
+
+  -- TODO: lspreference implemetieren
+  -- Telescope Search UI
+  {
+    "FabianWirth/search.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim" },
+    config = function()
+      local builtin = require('telescope.builtin')
+      require("search").setup({
+        mappings = {
+          next = "<Tab>",
+          prev = "<C-p>"
+        },
+        append_tabs = {
+          {
+            "Commits",
+            builtin.git_commits,
+            available = function()
+              return vim.fn.isdirectory(".git") == 1
+            end
+          }
+        },
+        tabs = {
+          {
+            "Files",
+            function(opts)
+              opts = opts or {}
+              if vim.fn.isdirectory(".git") == 1 then
+                builtin.git_files(opts)
+              else
+                builtin.find_files(opts)
+              end
+            end
+          },
+          {
+            name = "All Files",
+            tele_func = builtin.find_files,
+            tele_opts = { no_ignore = true, hidden = true }
+          },
+          {
+            -- Neuer Tab für Grep
+            name = "Grep",
+            tele_func = builtin.live_grep, -- Verwende `live_grep` für Grep durch alle Dateien
+          },
+          {
+            -- Neuer Tab für Buffers
+            name = "Buffers",
+            tele_func = builtin.buffers, -- Verwende `buffers` zum Durchsuchen der offenen Buffer
+          },
+        },
+        collections = {
+          -- Die "git" Sammlung, wie im ursprünglichen Setup
+          git = {
+            initial_tab = 1, -- Git branches
+            tabs = {
+              { name = "Branches", tele_func = builtin.git_branches },
+              { name = "Commits",  tele_func = builtin.git_commits },
+              { name = "Stashes",  tele_func = builtin.git_stash },
+            }
+          }
+        }
+      })
+    end
+  },
+
+  {
+    "ibhagwan/fzf-lua",
+    lazy = false,
+  },
+
+
+-- 2. File management
+
+  -- Harpoon for managing multiple files and quick navigation
+  {
+    "ThePrimeagen/harpoon",
+    lazy = false,                          -- Load immediately
+    dependencies = {
+      { "nvim-telescope/telescope.nvim" }, -- Telescope integration
+      { "nvim-lua/plenary.nvim" },
+    },
+    config = function()
+      require("harpoon").setup()
+      require("telescope").load_extension("harpoon")
+    end,
+  },
+
+
+-- 3. LSP completion
+-- TODO
   {
     "stevearc/conform.nvim",
     -- event = 'BufWritePre', -- uncomment for format on save
@@ -13,13 +128,214 @@ return {
     end,
   },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+
+-- 4. Git related plugins
+
+-- TODO: Optimales gut plugin finden
+  -- Fugitive for Git commands within Neovim
+  {
+    "tpope/vim-fugitive",
+    lazy = false, -- Stelle sicher, dass Fugitive sofort geladen wird
+  },
+
+  -- LazyGit integration for terminal Git UI
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+    }
+  },
+
+  -- Neogit for a Magit-like Git interface
+  {
+    "TimUntersberger/neogit",
+    requires = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("neogit").setup()
+    end,
+  },
+
+  -- DiffView for visualizing Git diffs
+  {
+    "sindrets/diffview.nvim",
+  requires = { "nvim-lua/plenary.nvim" }
+  },
+
+
+--  5. Dockertools
+
+  -- Dockertools
+  {
+    "kkvh/vim-docker-tools",
+    lazy = false,
+    config = function()
+    -- Anpassung der Anzeigeformate (optional)
+    vim.g.dockertools_container_format = 'table {{.ID}}\t{{.Image}}\t{{.Status}}\t{{.Names}}'
+    end,
+  },
+
+  -- Denops Docker Integration
+  {
+    "vim-denops/denops.vim",
+    lazy = false, -- Lade es sofort, um sicherzustellen, dass es verfügbar ist
+    config = function()
+      -- Optional: Überprüfen, ob Deno korrekt funktioniert
+      vim.cmd [[
+        command! DenopsCheckHealth echo "Denops Health Check"
+      ]]
+    end,
+  },
+  -- Denops Docker Integration
+  {
+    "skanehira/denops-docker.vim",
+    lazy = false,
+    dependencies = { "vim-denops/denops.vim" },
+    config = function()
+      -- Lade den Docker-Teil des Plugins
+      vim.cmd [[
+        command! DenopsReload call denops#server#restart()
+      ]]
+    end,
+  },
+
+
+-- 6. Markdown
+
+  -- Markdown Preview
+  {
+    "iamcco/markdown-preview.nvim",
+    ft = { "markdown" },
+    build = "cd app && npm install",
+  },
+
+  -- Checkboxes, Tabellen-Highlighting, und mehr
+  {
+    "preservim/vim-markdown",
+    ft = { "markdown" },
+    config = function()
+    end,
+  },
+
+  -- Headlines, Blöcke hervorheben
+  {
+    "lukas-reineke/headlines.nvim",
+    ft = { "markdown" },
+    config = function()
+      require("headlines").setup()
+    end,
+  },
+
+
+-- 7. Code formatting, folding, and syntax highlighting
+
+  -- Better Quickfix Window for better quickfix window
+  {
+    "kevinhwang91/nvim-bqf",
+    ft = 'qf',
+    opts = {
+      auto_enable = true,
+      auto_resize_height = true, -- Automatically adjust the height of the quickfix window
+    },
+    config = function(_, opts)
+      require("bqf").setup(opts)
+    end,
+  },
+
+  -- TODO: Was macht das
+  -- Quickfix Enhancer
+  {
+    "ashfinal/qfview.nvim",
+    event = "UIEnter",
+    config = function()
+      require("qfview").setup()
+    end,
+  },
+
+  -- Auto Template Strings
+  {
+    "chrisgrieser/nvim-puppeteer",
+    lazy = false, -- plugin lazy-loads itself. Can also load on filetypes.
+  },
+
+  -- TODO: Erklären was das macht, wie man das weiter erweitern kann, wie man es anwendet im workflow
+  -- TODO: ctrl / sollte das machen
+  {
+    "numToStr/Comment.nvim",
+    keys = {
+      { "gcc", mode = "n",          desc = "Comment toggle current line" },
+      { "gc",  mode = { "n", "o" }, desc = "Comment toggle linewise" },
+      { "gc",  mode = "x",          desc = "Comment toggle linewise (visual)" },
+      { "gbc", mode = "n",          desc = "Comment toggle current block" },
+      { "gb",  mode = { "n", "o" }, desc = "Comment toggle blockwise" },
+      { "gb",  mode = "x",          desc = "Comment toggle blockwise (visual)" },
+    },
+    config = function(_, opts)
+      require("Comment").setup(opts) -- Initialisiert das Plugin
+    end,
+  },
+
+  -- TODO: In nvim.cmp integirieren!
+   -- GitHub Copilot integration
+   {
+    "github/copilot.vim",
+    event = "InsertEnter",
+    config = function()
+      vim.g.copilot_no_tab_map = true -- Prevent conflicts with tab mappings
+      vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+    end,
+  },
+
+  -- Use treesitter to autoclose and autorename html tag
+  {
+    "windwp/nvim-ts-autotag",
+    opts = function()
+      return {
+        enable_close = true,           -- Auto close tags
+        enable_rename = true,          -- Auto rename pairs of tags
+        enable_close_on_slash = false, -- Auto close on trailing </
+        per_filetype = {
+          ["html"] = {
+            enable_close = false
+          }
+        }
+      }
+    end,
+    config = function(_, opts)
+      require("nvim-ts-autotag").setup(opts)
+    end,
+  },
+
+
+-- 7.1 Coding JavaScript / TypeScript
+
+
+
+
+-- 8. MISC Plugins
+
+   -- Zen Mode
+   {
+    "folke/zen-mode.nvim",
+    },
+
+    -- TMUX
+    {
+        "aserowy/tmux.nvim",
+        config = function() return require("tmux").setup() end
+    },
+
 }
