dc6fa52272e1fd559f410d229f79214979a0d3bb
Stefan Bartl <bartl.stefanwien@gmail.com>
2025-07-24 05:57:43 +0200

    Solved bug with false separators in windows environments

--- lua/custom/workspace.lua
+++ lua/custom/workspace.lua
@@ -17,13 +17,20 @@ vim.keymap.set("n", "<leader>ow", open_workspace_layout, { desc = "[Custom] Work
 -- Zeigt den Pfad beim Öffnen oder Wechseln eines Buffers, aber nur für Dateibuffer
 vim.api.nvim_create_autocmd("BufEnter", {
   callback = function()
-    -- Prüft, ob der Buffer ein Dateibuffer ist
     if vim.bo.buftype == "" then
-      local file_path = vim.fn.expand("%:p")
-      if file_path ~= "" then
-        vim.cmd('echo "' .. file_path .. '"')
+      local curr_buf = vim.api.nvim_get_current_buf()
+      local curr_win = vim.api.nvim_get_current_win()
+      local win_buf = vim.api.nvim_win_get_buf(curr_win)
+
+      if curr_buf == win_buf then
+        local path = vim.fn.expand("%:p"):gsub("\\", "/")
+        if path ~= "" then
+          vim.notify(path)
+        end
       end
     end
   end,
-  desc = "Shows absolute path of the file only once for file buffers",
+  desc = "Shows path for opening file buffer if visible.",
 })
+
+
