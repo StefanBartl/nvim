71a27866e3f1e8b3e111f258c76de0d754868d85
Stefan Bartl <bartl.stefanwien@gmail.com>
2025-07-24 06:03:01 +0200

    debugged multiroot

--- lua/custom/fd_multi_root.lua
+++ lua/custom/fd_multi_root.lua
@@ -3,27 +3,40 @@ local finders = require("telescope.finders")
 local previewers = require("telescope.previewers")
 local conf = require("telescope.config").values
 local Job = require("plenary.job")
+local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
 
 local M = {}
+
+-- Hilfsfunktion: macht aus einem String-Pfad einen Telescope-Entry
+local function make_entry(path)
+  return {
+    value = path,
+    display = path,
+    ordinal = path,
+    path = path,
+  }
+end
 
 --- Run `fd` in multiple roots and preview results using Telescope
 ---@async
 function M.fd_multi_root()
   local search_dirs = {
-    "C:/Users",     -- Erstes Verzeichnis
-    "E:/MyGithub",  -- Zweites Verzeichnis
+    "C:/Users",    -- Beispiel 1
+    "E:/MyGithub", -- Beispiel 2
   }
 
-  local all_results = {}        -- Sammlung aller Dateipfade
+  local all_results = {}
   local jobs_remaining = #search_dirs
 
   for _, dir in ipairs(search_dirs) do
     Job:new({
-      command = "fd",
+      command = "fd", -- ggf. "fd.exe" unter Windows
       args = { "--type", "f", "--hidden", "--exclude", ".git" },
       cwd = dir,
       on_exit = function(j)
         for _, line in ipairs(j:result()) do
+          -- fd gibt relative Pfade zurück → auf absoluten Pfad mappen
           table.insert(all_results, dir .. "/" .. line)
         end
 
@@ -32,9 +45,24 @@ function M.fd_multi_root()
           vim.schedule(function()
             pickers.new({}, {
               prompt_title = "Find files (multi root)",
-              finder = finders.new_table({ results = all_results }),
+              finder = finders.new_table {
+                results = all_results,
+                entry_maker = make_entry,
+              },
               sorter = conf.generic_sorter({}),
-              previewer = previewers.cat.new({}),  -- Vorschau hinzufügen
+              previewer = previewers.cat.new({}),
+              attach_mappings = function(prompt_bufnr, map)
+                local function open_file()
+                  local entry = action_state.get_selected_entry()
+                  actions.close(prompt_bufnr)
+                  -- fnameescape für Leerzeichen etc.
+                  vim.cmd("edit " .. vim.fn.fnameescape(entry.path or entry.value))
+                end
+
+                map("i", "<CR>", open_file)
+                map("n", "<CR>", open_file)
+                return true
+              end,
             }):find()
           end)
         end
