79ca56b3c53ee48099cfed140a50800c68b1aa89
Stefan Bartl <bartl.stefan@pm.me>
2025-08-23 20:34:07 +0200

    updated fzf and telescope mappings

--- lua/mappings/fzf_mappings.lua
+++ lua/mappings/fzf_mappings.lua
@@ -17,25 +17,16 @@ function M.setup()
   map("n", "<leader>do", ":FzfLua diagnostics_document<CR>", { desc = "[FzfLua] Document Diagnostics" })
   map("n", "<leader>wo", ":FzfLua diagnostics_workspace<CR>", { desc = "[FzfLua] Workspace Diagnostics" })
 
-  -- LSP quick pickers
-  map("n", "<leader>flr", ":FzfLua lsp_references<CR>", { desc = "[FzfLua] References" })
-  map("n", "<leader>flt", ":FzfLua lsp_typedefs<CR>", { desc = "[FzfLua] Typedefs" })
-  map("n", "<leader>fli", ":FzfLua lsp_implementations<CR>", { desc = "[FzfLua] Implementations" })
-
-  -- Registers & changes
-  map("n", "<leader><", ":FzfLua registers<CR>", { desc = "[FzfLua] Registers" })
-
   -- Quickfix & man-pages
-  map("n", "<leader>fqf", ":FzfLua quickfix<CR>", { desc = "[Quickfix] Quickfix" })
+  map("n", "<leader>fq", ":FzfLua quickfix<CR>", { desc = "[Quickfix] Quickfix" })
   map("n", "<leader>man", ":FzfLua man_pages<CR>", { desc = "[FzfLua] Man Pages" })
 
   -- Search
   map("n", "<leader>flg", ":FzfLua live_grep<CR>", { desc = "[FzfLua] Live Grep" })
   map("n", "<leader>fgp", ":FzfLua grep<CR>", { desc = "[FzfLua] Grep" })
-  map("n", "<leader>fbb", "<cmd>FzfLua grep_curbuf<CR>", { desc = "[FzfLua] Grep current buffer" })
+  map("n", "<leader>fgb", "<cmd>FzfLua grep_curbuf<CR>", { desc = "[FzfLua] Grep current buffer" })
 
-  -- Filetypes
-  map("n", "<leader>fil", ":FzfLua filetypes<CR>", { desc = "[FzfLua] Filetypes" })
+  map("n", "<leader>old", ":FzfLua oldfiles<CR>", { desc = "[FzfLua] Oldfiles" })
 end
 
 return M
--- lua/mappings/telescope_mappings.lua
+++ lua/mappings/telescope_mappings.lua
@@ -5,32 +5,29 @@ local M = {}
 function M.setup()
   local map = vim.g.__map_helper
 
+  map("n", "<leader>os", function() require("search").open() end, { desc = "[Telescope] Multi search UI" })
   map("n", "<leader>ts", ":Telescope<CR>", { desc = "[Telescope] UI" })
 
-  -- Prompted grep
   map("n", "<leader>gs", function()
     local ok, tb = pcall(require, "telescope.builtin")
     if not ok then return end
     tb.grep_string({ search = vim.fn.input("Grep > ") })
   end, { desc = "[Telescope] Grep" })
 
-  -- Insert-mode meta shortcuts (kept as in source)
   map("i", "<M-p>", function() pcall(require("telescope.builtin").find_files) end, { desc = "[Telescope] Prev cat" })
   map("i", "<M-n>", function() pcall(require("telescope.builtin").find_files) end, { desc = "[Telescope] Next cat" })
 
-  map("n", "<leader>fw", "<cmd>Telescope live_grep<CR>", { desc = "[Telescope] Live Grep" })
-  map("n", "<leader>fk", "<cmd>Telescope keymaps<CR>", { desc = "[Telescope] Find keymaps" })
-  map("n", "<leader>fb", "<cmd>Telescope buffers<CR>", { desc = "[Telescope] Buffers" })
-  map("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", { desc = "[Telescope] Help" })
-  map("n", "<leader>fo", "<cmd>Telescope oldfiles<CR>", { desc = "[Telescope] Oldfiles" })
-  map("n", "<leader>fz", "<cmd>Telescope current_buffer_fuzzy_find<CR>", { desc = "[Telescope] In Buffer" })
-  map("n", "<leader>cm", "<cmd>Telescope git_commits<CR>", { desc = "[Telescope] Git Commits" })
-  map("n", "<leader>gt", "<cmd>Telescope git_status<CR>", { desc = "[Telescope] Git Status" })
-  map("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "[Telescope] Find Files" })
-  map("n", "<leader>fa", "<cmd>Telescope find_files follow=true no_ignore=true hidden=true<CR>",
+  map("n", "<leader>tlg", "<cmd>Telescope live_grep<CR>", { desc = "[Telescope] Live Grep" })
+  map("n", "<leader>tkey", "<cmd>Telescope keymaps<CR>", { desc = "[Telescope] Find keymaps" })
+  map("n", "<leader>tb", "<cmd>Telescope buffers<CR>", { desc = "[Telescope] Buffers" })
+  map("n", "<leader>fhelp", "<cmd>Telescope help_tags<CR>", { desc = "[Telescope] Help" })
+  map("n", "<leader>told", "<cmd>Telescope oldfiles<CR>", { desc = "[Telescope] Oldfiles" })
+  map("n", "<leader>tcb", "<cmd>Telescope current_buffer_fuzzy_find<CR>", { desc = "[Telescope] In Buffer" })
+  map("n", "<leader>tcm", "<cmd>Telescope git_commits<CR>", { desc = "[Telescope] Git Commits" })
+  map("n", "<leader>tg", "<cmd>Telescope git_status<CR>", { desc = "[Telescope] Git Status" })
+  map("n", "<leader>tff", "<cmd>Telescope find_files<cr>", { desc = "[Telescope] Find Files" })
+  map("n", "<leader>tfa", "<cmd>Telescope find_files follow=true no_ignore=true hidden=true<CR>",
     { desc = "[Telescope] Find All Files" })
-
-  map("n", "<leader>os", function() require("search").open() end, { desc = "[Telescope] Multi search UI" })
 end
 
 return M
