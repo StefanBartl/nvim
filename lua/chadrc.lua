fa2ceb0648296956efcd76d7b7a353116a782f75
Stefan Bartl <bartl.stefan@pm.me>
2025-08-24 02:27:24 +0200

    ld

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -9,7 +9,7 @@ M.ui = {
 }
 
 M.base46 = {
-  transparency = false,
+   transparency = false,
   --theme = "onedark",
   --theme = "default-dark",
   --theme = "github_dark",
@@ -31,13 +31,5 @@ if vim.g.is_windows and vim.g.is_pwsh then
   vim.opt.shellquote = ""
   vim.opt.shellxquote = ""
 end
-
-require("keys").setup {
-    enable_on_startup = true,
-    win_opts = {
-        width = 50,
-        focusable = false,
-    },
-}
 
 return M
--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -25,13 +25,14 @@ return {
   },
   ]]--
 
-  ---@type LazyPluginSpec
-  {
-    'brianhuster/live-preview.nvim',
-    dependencies = {
-      'ibhagwan/fzf-lua',
-    },
-    lazy = false,
-  },
+{
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  build = "cd app && yarn install",
+  init = function()
+    vim.g.mkdp_filetypes = { "markdown" }
+  end,
+  ft = { "markdown" },
+},
 
 }
