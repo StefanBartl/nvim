02f3f58d54d6b66e4e9d3da2c072b8fc8197b887
Stefan Bartl <bartl.stefan@pm.me>
2025-08-24 22:52:09 +0200

    fzf and telescopoe usr picker implemented

---
+++ lua/@types/usrcmds.lua
@@ -0,0 +1,19 @@
+---@alias DirPath string
+
+---@class UsrPickersKeymaps
+---@field tel_files string
+---@field tel_grep  string
+---@field fzf_files string
+---@field fzf_grep  string
+
+---@class UsrPickersCommands
+---@field find_files_telescope string
+---@field grep_telescope       string
+---@field find_files_fzf       string
+---@field grep_fzf             string
+
+---@class UsrPickersConfig
+---@field keys UsrPickersKeymaps
+---@field commands UsrPickersCommands
+---@field enable_keymaps boolean
+---@field notify_level integer
